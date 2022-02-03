64f9cc1e8517b471ca82c8ce7a104d17cd260785
Sam Olukotun <sam.olukotun@circleci.com>
2022-02-02 19:41:47 -0800

    Add files for batch functions

---
+++ examples/followManyProjects.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/olukotun-ts/circleci-client-go/circleci"
+)
+
+func main() {
+	ctx := context.Background()
+	c := circleci.NewClient(nil)
+	
+	slugs := []string{
+		"gh/olukotun-ts/confluent-kafka-go", 
+		"gh/olukotun-ts/circleci-demo", 
+		"gh/olukotun-ts/circleci-demo-ruby-rails",
+	}
+
+	responses, err := c.Projects.FollowMany(ctx, slugs, "master")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, res := range responses {
+		log.Print("Request completed with: ", res.Status)
+	}
+}
+
